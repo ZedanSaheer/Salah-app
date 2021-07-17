870f42ff5a0f751c4e786d09e743bbafee501895
Zedan <zedansaheer.dev@gmail.com>
2021-07-17 17:52:33 +0530

    version 1.0 complete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dhuhr = document.querySelector(".prayer-dhuhr");
 const asr = document.querySelector(".prayer-asr");
 const maghrib = document.querySelector(".prayer-maghrib");
 const isha = document.querySelector(".prayer-isha");
+const hijriDateContainer = document.querySelector(".hijri-date")
+const gregDateContainer = document.querySelector(".greg-date")
 
 window.onload = async () => {
     const response = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=9f8ad84b34fd4f60b3de2640038168f5`);
@@ -20,7 +22,9 @@ async function updateSalah(data){
     const dataSalah = await responseSalah.json();
     const currentData = dataSalah.data[date-1];
     updateTimings(currentData);
+    console.log(currentData.date);
 }
+
 
 function updateTimings(currentData){
     fajr.innerHTML = currentData.timings.Fajr;
@@ -28,6 +32,10 @@ function updateTimings(currentData){
     asr.innerHTML = currentData.timings.Asr;
     maghrib.innerHTML = currentData.timings.Maghrib;
     isha.innerHTML = currentData.timings.Isha;
+    
+    gregDateContainer.innerHTML = "The Gregorian Calender : " + currentData.date.readable + ", " + currentData.date.gregorian.weekday.en  
+
+    hijriDateContainer.innerHTML = "The Islamic Calender : "+currentData.date.hijri.day + " " + currentData.date.hijri.month.ar + " " + currentData.date.hijri.year + ", " + currentData.date.hijri.weekday.ar
 }
 
 
